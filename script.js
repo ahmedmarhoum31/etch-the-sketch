5c2ddf173cc76ea274f90ea42be03afbff9c71a6
ahmedmarhoum31 <marhoum80@yahoo.com>
2026-07-23 17:41:16 +0100

    add customizable grid size and hover drawing
    - Add button to customize grid dimensions
    - Implement function to regenerate the grid
    - Remove previous grid before creating a new one
    - Calculate cell size dynamically based on user input
    - Add hover effect to draw on grid cells
    - Update layout and button styling

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const container = document.querySelector(".grid-container");
 let gridSize = 16;
 let maximumGridSize = String(60/16)+"vh";
 
+//this for loop will create 16 div container, in which each container will have 16 divs inside of them
 for(let i=0;i<16;i++)
 {
     let newDivContainer = document.createElement("div");
@@ -16,5 +17,46 @@ for(let i=0;i<16;i++)
        gridElement.style.height = maximumGridSize;
        gridElement.style.aspectRatio ="1/1";
        newDivContainer.appendChild(gridElement);
+    }
+}
+
+const gridElements = document.querySelectorAll(".grid-element");
+
+gridElements.forEach(element => element.addEventListener("mouseenter",()=>
+{
+    element.style.backgroundColor = "black";
+
+}))
+
+function settingTheGrid(userInput)
+{
+    if(userInput>=100)
+    {
+        prompt("maximum greed number is 100!");
+
+    }else
+    {
+        //removing previous Grid
+        let existingGridElements = document.querySelectorAll("make-container-flex");
+        existingGridElements.forEach(div=> div.remove());
+
+        //calculating Grid size based on grid number
+        let maximumGridSize = String(60/userInput)+"vh";
+
+        for(let i = 0; i<userInput; i++)
+        {
+            let newDivContainer = document.createElement("div");
+            newDivContainer.classList.add("make-container-flex");
+            container.appendChild(newDivContainer);
+            for(j = 0; j<userInput; j++)
+            {
+                let gridElement = document.createElement("div");
+                gridElement.classList.add("grid-element");
+                gridElement.style.height = maximumGridSize;
+                gridElement.style.aspectRatio ="1/1";
+                newDivContainer.appendChild(gridElement);
+            }
+        }
+        
     }
 }
