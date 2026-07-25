404a739df04f4cb7d7b78bc16453aafe3f7d1190
ahmedmarhoum31 <marhoum80@yahoo.com>
2026-07-25 13:48:40 +0100

    add event.preventDefault() in touchmove event listener so stop screen scroll

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ function settingTheGrid(userInput)
 
     container.addEventListener("touchmove",(event)=>
         {
+            event.preventDefault();
             console.log(event.changedTouches)
             const touchLocation = event.changedTouches[0];
             const currentTarget = document.elementFromPoint(touchLocation.clientX, touchLocation.clientY);
